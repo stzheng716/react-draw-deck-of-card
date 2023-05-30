1c4107d344d26529d957c993d8632f06d1832f1d
Steven <stzheng716@gmail.com>
2023-05-30 12:23:57 -0700

    add drawnCard function

--- src/Card.js
+++ src/Card.js
@@ -1,2 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { API_URL } from "./DeckOfCards";
+
+function Card({ card }){
+
+    return(
+        <div>
+            <img
+            src={`${card.image}`}
+            />
+        </div>
+    )
+}
+
+export default Card
--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -6,10 +6,10 @@ const API_URL = "https://deckofcardsapi.com/api";
 function DeckOfCards() {
   const [deck, setDeck] = useState({
     id: null,
-    isLoading: true,
+    isLoading: true
   });
 
-  const [card, setCard] = useState(null);
+  const [drawnCards, setCard] = useState([]);
 
   useEffect(function fetchDeckMounted() {
     async function fetchDeck() {
@@ -20,28 +20,30 @@ function DeckOfCards() {
       });
     }
     fetchDeck();
-  }, []);
+  }, [ ]);
 
-  if (deck.isLoading) return <i>Loading...</i>;
 
   async function drawCard() {
     const response = await axios.get(
       `${API_URL}/deck/${deck.id}/draw/?count=1`
     );
-    setCard(response.data.cards[0].code);
+    const newCard = response.data.cards[0]
+    setCard(cards => [...cards, newCard]);
+    if(response.data.remaining === 0) alert("Error: no cards remaining")
   }
 
   function handleClick() {
-    //
+    drawCard()
   }
+
+  if (deck.isLoading) return <i>Loading...</i>;
 
   return (
     <div>
       <button onClick={handleClick}>GIMME A CARD</button>
-      <Card />
+      {drawnCards.map(card => <Card key={card.code} card={card}/>)}
     </div>
   );
 }
 
 export default DeckOfCards;
-export { API_URL };
